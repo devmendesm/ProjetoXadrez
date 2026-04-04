db6685b1dc357a17e407f889b7a66f9ecfa9fbde
devmendesm <matheusviniciusmendes@gmail.com>
2026-04-04 16:08:02 -0300

    PRIMEIRO COMMIT

---
+++ ProjetoXadrez/Tela.cs
@@ -0,0 +1,27 @@
+﻿
+using tabuleiro;
+
+namespace ProjetoXadrez
+{
+    internal class Tela
+    {
+        public static void imprimirTabuleiro(Tabuleiro tab)
+        {
+            for (int i = 0; i < tab.linhas; i++)
+            {
+                for (int j = 0; j < tab.colunas; j++)
+                {
+                    if (tab.getPeca(i, j) == null)
+                    {
+                        Console.Write("- ");
+                    }
+                    else
+                    {
+                        Console.Write(tab.getPeca(i, j) + " ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
