64f6708280db856d3a523b60fff4e4f06297b39e
devmendesm <matheusviniciusmendes@gmail.com>
2026-04-05 17:32:04 -0300

    FEAT:classe PosicaoXadrez

---
+++ ProjetoXadrez/xadrez/PosicaoXadrez.cs
@@ -0,0 +1,28 @@
+﻿
+using tabuleiro;
+
+namespace xadrez
+{
+    internal class PosicaoXadrez
+    {
+        public char coluna { get; set; }
+        public int linha { get; set; }
+
+        public PosicaoXadrez(char coluna, int linha)
+        {
+            this.coluna = coluna;
+            this.linha = linha;
+        }
+
+        //Método para converter a posição para uma posição normal do tabuleiro
+        public Posicao toPosicao()
+        {
+            return new Posicao(8 - linha, coluna - 'a');
+        }
+
+        public override string ToString()
+        {
+            return "" + coluna + linha;
+        }
+    }
+}
