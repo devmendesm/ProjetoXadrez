1e294417bfea8e671aa6e6d109b83d2df96cc194
devmendesm <matheusviniciusmendes@gmail.com>
2026-04-05 17:42:06 -0300

    FEAT:função de imprimirPeca de acordo com a cor

--- ProjetoXadrez/Tela.cs
+++ ProjetoXadrez/Tela.cs
@@ -9,6 +9,7 @@ namespace ProjetoXadrez
         {
             for (int i = 0; i < tab.linhas; i++)
             {
+                Console.Write(8 - i + " ");
                 for (int j = 0; j < tab.colunas; j++)
                 {
                     if (tab.getPeca(i, j) == null)
@@ -17,10 +18,27 @@ namespace ProjetoXadrez
                     }
                     else
                     {
-                        Console.Write(tab.getPeca(i, j) + " ");
+                        imprimirPeca(tab.getPeca(i, j));
+                        Console.Write(" ");
                     }
                 }
                 Console.WriteLine();
+            }
+            Console.WriteLine("  a b c d e f g h");
+        }
+
+        public static void imprimirPeca(Peca peca)
+        {
+            if (peca.cor == Cor.Branca)
+            {
+                Console.Write(peca);
+            }
+            else
+            {
+                ConsoleColor aux = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write(peca);
+                Console.ForegroundColor = aux;
             }
         }
     }
