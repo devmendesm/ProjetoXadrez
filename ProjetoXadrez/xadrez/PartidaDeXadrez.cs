759bfb4de843671cb0e3e85d553d7916da8c000f
devmendesm <matheusviniciusmendes@gmail.com>
2026-04-25 17:57:28 -0300

    FEAT:implementação das jogadas especiais roque pequeno e grande

--- ProjetoXadrez/xadrez/PartidaDeXadrez.cs
+++ ProjetoXadrez/xadrez/PartidaDeXadrez.cs
@@ -38,6 +38,26 @@ namespace xadrez
                 capturadas.Add(pecaCapturada);
             }
 
+            // #JOGADAESPECIAL ROQUE PEQUENO
+            if (p is Rei && destino.coluna == origem.coluna + 2)
+            {
+                Posicao origemT = new Posicao(origem.linha, origem.coluna + 3);
+                Posicao destinoT = new Posicao(origem.linha, origem.coluna + 1);
+                Peca T = tab.retirarPeca(origemT);
+                T.incrementarQteMovimentos();
+                tab.colocarPeca(T, destinoT);
+            }
+
+            // #JOGADAESPECIAL ROQUE GRANDE
+            if (p is Rei && destino.coluna == origem.coluna - 2)
+            {
+                Posicao origemT = new Posicao(origem.linha, origem.coluna - 4);
+                Posicao destinoT = new Posicao(origem.linha, origem.coluna - 1);
+                Peca T = tab.retirarPeca(origemT);
+                T.incrementarQteMovimentos();
+                tab.colocarPeca(T, destinoT);
+            }
+
             return pecaCapturada;
         }
 
@@ -51,6 +71,26 @@ namespace xadrez
                 capturadas.Remove(pecaCapturada);
             }
             tab.colocarPeca(p, origem);
+
+            // #JOGADAESPECIAL ROQUE PEQUENO
+            if (p is Rei && destino.coluna == origem.coluna + 2)
+            {
+                Posicao origemT = new Posicao(origem.linha, origem.coluna + 3);
+                Posicao destinoT = new Posicao(origem.linha, origem.coluna + 1);
+                Peca T = tab.retirarPeca(destinoT);
+                T.decrementarQteMovimentos();
+                tab.colocarPeca(T, origemT);
+            }
+
+            // #JOGADAESPECIAL ROQUE GRANDE
+            if (p is Rei && destino.coluna == origem.coluna - 2)
+            {
+                Posicao origemT = new Posicao(origem.linha, origem.coluna - 4);
+                Posicao destinoT = new Posicao(origem.linha, origem.coluna - 1);
+                Peca T = tab.retirarPeca(destinoT);
+                T.decrementarQteMovimentos();
+                tab.colocarPeca(T, origemT);
+            }
         }
 
         public void realizaJogada(Posicao origem, Posicao destino)
--- ProjetoXadrez/xadrez/Rei.cs
+++ ProjetoXadrez/xadrez/Rei.cs
@@ -24,6 +24,12 @@ namespace xadrez
             return p == null || p.cor != cor;
         }
 
+        private bool testeTorreParaRoque(Posicao pos)
+        {
+            Peca p = tab.getPeca(pos);
+            return p != null && p is Torre && p.cor == cor && p.qteMovimentos == 0;
+        }
+
         public override bool[,] movimentosPossiveis()
         {
             bool[,] mat = new bool[tab.linhas, tab.colunas];
@@ -85,6 +91,40 @@ namespace xadrez
             {
                 mat[pos.linha, pos.coluna] = true;
             }
+
+            // # JOGADAESPECIAL ROQUE
+            if (qteMovimentos == 0 && !partida.xeque)
+            {
+                // ROQUE PEQUENO
+                Posicao posT1 = new Posicao(posicao.linha, posicao.coluna + 3);
+                 
+                if (testeTorreParaRoque(posT1))
+                {
+                    Posicao p1 = new Posicao(posicao.linha, posicao.linha + 1);
+                    Posicao p2 = new Posicao(posicao.linha, posicao.linha + 2);
+
+                    if (tab.getPeca(p1) == null && tab.getPeca(p2) == null)
+                    {
+                        mat[posicao.linha, posicao.coluna + 2] = true;
+                    }
+                }
+
+                // ROQUE GRANDE
+                Posicao posT2 = new Posicao(posicao.linha, posicao.coluna - 4);
+                 
+                if (testeTorreParaRoque(posT1))
+                {
+                    Posicao p1 = new Posicao(posicao.linha, posicao.linha - 1);
+                    Posicao p2 = new Posicao(posicao.linha, posicao.linha - 2);
+                    Posicao p3 = new Posicao(posicao.linha, posicao.linha - 3);
+
+                    if (tab.getPeca(p1) == null && tab.getPeca(p2) == null && tab.getPeca(p3) == null)
+                    {
+                        mat[posicao.linha, posicao.coluna - 2] = true;
+                    }
+                }
+            }
+
 
             return mat;
         }
